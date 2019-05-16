3acf267285e5aa0c3048aca20230b2ad25a820bd
陈文辉 <3418706125@qq.com>
2019-05-16 10:57:14 +0800

    IOC&Autofac
    依赖注入Demo

---
+++ IOC/IOC/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC
+{
+    public class Test : ITest
+    {
+        public string Add()
+        {
+            return "向Mysql数据库添加成功！";
+        }
+    }
+}
---
+++ IOC/Services/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Services
+{
+    public class Test : ITest
+    {
+        public string Add()
+        {
+            return "添加成功！";
+        }
+    }
+}
